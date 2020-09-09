af13ac948c01d4b7f8e6921c93f07e86a1a08a8e
terrylinda13@gmail.com <terrylinda13@gmail.com>
2020-09-08 18:42:59 -0700

    Revert "cmpe 272"
    This reverts commit 75e1f574e5f27b9f4880354e692e0692b3acf4ab.

--- TutorMe/shubham_curl.php
+++
@@ -1,9 +0,0 @@
-<?php
-$ch = curl_init("http://www.shubhamzingh.tech/marketplace/getTopProducts.php");
-$fp = fopen("example_homepage.txt", "w");
-
-curl_setopt($ch, CURLOPT_FILE, $fp);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-
-curl_exec($ch);
-?>
--- addUser.php
+++
@@ -1,9 +0,0 @@
-<?php require "dbConnect.php"?>
-<?php 
-   
-    $sql = "INSERT INTO company_users VALUES ('".$_POST["fname"]."', '".$_POST["lname"]."', '".$_POST["email"]."', '".$_POST["address"]."', '".$_POST["hphone"]."', '".$_POST["lphone"]."')";
-    $conn->query($sql);
-    $conn->close();
-    header("location: users.php");
-    
-?>
